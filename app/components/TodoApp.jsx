0aec47beac72cb3ef32d0b1241673da3a1926a75
3225297@gmail.com <3225297@gmail.com>
2017-03-22 23:12:26 +0800

    add TodoApp

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+   render : function() {
+     return (
+       <h3> Todo app </h3>
+     );
+   }
+});
+
+module.exports = TodoApp;
---
+++ app/tests/TodoApp.test.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jQuery');
+
+var TodoApp = require('TodoApp');
+
+describe('TodoApp', () => {
+
+    it('Shoud exist', ()=>{
+        expect(TodoApp).isExist();
+    });
+
+});
