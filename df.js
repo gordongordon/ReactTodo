8d7d29335e3a6bcb92ce105e2c8173f5a55b377b
3225297@gmail.com <3225297@gmail.com>
2017-03-27 14:10:56 +0800

    add showCompleted and searchText reducer

---
+++ df.js
@@ -0,0 +1,11 @@
+var deepFreeze = require('deep-freeze-strict');
+
+Buffer.x = 1;
+deepFreeze(Buffer);
+
+Buffer.x = 5;
+
+console.log(Buffer.x === undefined);
+
+Buffer.prototype.z = 3;
+console.log(Buffer.prototype.z === undefined);
