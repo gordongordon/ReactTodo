7bee0d7ed4da21c7a695e65d36628cf380835804
3225297@gmail.com <3225297@gmail.com>
2017-04-04 00:45:52 +0800

    add config process.env

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,12 +3,12 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBNAUoza_QB9F-OmM4hO2Lze7K2s3_7T3o",
-    authDomain: "gd-todo-app.firebaseapp.com",
-    databaseURL: "https://gd-todo-app.firebaseio.com",
-    storageBucket: "gd-todo-app.appspot.com",
-    messagingSenderId: "1037265767093"
-  };
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+//    databaseURL: "https://todo-app-test-16958.firebaseio.com",
+   };
   firebase.initializeApp(config);
 } catch (e) {
 
